383bf3fe216df58efb451d91961914a7942e4495
Luciano Pardo <pardobio@gmail.com>
2025-06-09 12:54:20 -0300

    fix: implementar debounce en la barra de búsqueda

--- client/src/components/UserCRUD/SearchBox.jsx
+++ client/src/components/UserCRUD/SearchBox.jsx
@@ -1,23 +1,41 @@
-import React from 'react';
+import { React, useRef } from 'react';
 import { Input } from 'antd';
 import { useUserContext } from '../../context/UserContext';
 
 const { Search } = Input;
 
+// Función debounce para evitar múltiples llamadas a la API
+    const debounce = (fx, delay) => {
+        let timeoutId;
+        return (...args) => {
+            if (timeoutId) clearTimeout(timeoutId);
+            timeoutId = setTimeout(() => {
+                fx(...args);
+            }, delay);
+        };
+    }; 
+
 const SearchBox = () => {
     const { setFilterAndReload, filters } = useUserContext();
 
-    const onSearch = (query) => {
-        setFilterAndReload({
-            ...filters,
-            name: query,
-        });
+    const debounceRef = useRef(
+        debounce((query) => {
+            setFilterAndReload({
+                ...filters,
+                name: query,
+            });
+        }, 500)
+    );
+    
+    const handleChange = (e) => {
+        const query = e.target.value;
+        debounceRef.current(query);
     };
 
     return (
         <Search
             placeholder="Buscar Usuarios"
-            onSearch={onSearch}
+            onChange={handleChange}
             allowClear
             style={{
                 width: 300,
